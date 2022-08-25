c5b0453f58948c8be2d33bf5cc80e9597d1fccb1
Ramiro <ramifazio@gmail.com>
2022-08-24 21:32:24 -0300

    Sugerencias!

--- script.js
+++ script.js
@@ -1,66 +1,49 @@
-let monedas=[0,0,0,0,0,0];
-let valores=[50,20,10,5,2,1]
-let monedas_texto=[];
+let monedas = [0, 0, 0, 0, 0, 0];
+let valores = [50, 20, 10, 5, 2, 1];
+let monedas_texto = [];
 
-let retorno=prompt("Digita el valor a retornar")
+let retorno = prompt("Digita el valor a retornar");
 let contador;
 let residuo;
 
+class Calculadora {
+  constructor(retorno, valores) {
+    this.retorno = parseInt(retorno);
+    this.valores = valores;
+  }
 
-class Calculadora{
-    
-    constructor(retorno, valores){
-            this.retorno=parseInt(retorno);
-            this.valores=valores;
+  getCoins() {
+    let n = 0;
+
+    while (n < 5) {
+      let moneda = retorno / valores[n];
+      contador = parseInt(moneda);
+      monedas[n] = contador;
+      residuo = retorno - contador * valores[n];
+
+      if (residuo == 0) {
+        break;
+      } else {
+        retorno = residuo;
+        //console.log(retorno);
+      }
+
+      n++;
     }
 
-    getCoins(){
-        let n=0;
-        
-        while(n<5){
+    this.getTexto_monedas(monedas);
+  }
 
-            
-            let moneda=retorno/valores[n];
-            contador=parseInt(moneda);
-            monedas[n]=contador;
-            residuo=retorno - (contador*valores[n])
-
-                if(residuo==0){
-                  break;
-                }
-                else{
-                  retorno = residuo;
-                  //console.log(retorno);
-                }
-      
-           
-            n++;
-
-        }
-            
-            this.getTexto_monedas(monedas);
-           
+  getTexto_monedas(monedas_final) {
+    for (let i = 0; i < monedas_final.length; i++) {
+      if (monedas_final[i] != 0) {
+        monedas_texto[i] = monedas_final[i] + " monedas de " + valores[i];
+      }
     }
 
-    getTexto_monedas(monedas_final){
-      
-        for(let i=0; i<monedas_final.length; i++){
-
-          if(monedas_final[i]!=0){
-
-          monedas_texto[i]=monedas_final[i] + " monedas de " + valores[i];
-            
-          }
-        
-        }
-
-        document.getElementById(`resultado`).innerHTML =monedas_texto;
-    }
-
-
+    document.getElementById(`resultado`).innerHTML = monedas_texto;
+  }
 }
 
 const vueltas = new Calculadora(retorno, valores);
 vueltas.getCoins();
-
-
